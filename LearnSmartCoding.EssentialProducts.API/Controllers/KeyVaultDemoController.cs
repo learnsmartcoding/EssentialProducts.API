e421612ef04ed383479e525fd44142787ddb83cb
Learn Smart Coding <learnsmartcoding@gmail.com>
2023-01-03 16:54:48 -0500

    KV changes

--- LearnSmartCoding.EssentialProducts.API/Controllers/KeyVaultDemoController.cs
+++ LearnSmartCoding.EssentialProducts.API/Controllers/KeyVaultDemoController.cs
@@ -1,10 +1,13 @@
 ﻿using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using Azure.Security.KeyVault.Keys;
 using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace LearnSmartCoding.EssentialProducts.API.Controllers
@@ -27,8 +30,6 @@ namespace LearnSmartCoding.EssentialProducts.API.Controllers
         }
 
 
-
-
         [HttpGet("GetCustomSecret", Name = "GetCustomSecret")]
         [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetCustomSecretAsync()
@@ -42,7 +43,7 @@ namespace LearnSmartCoding.EssentialProducts.API.Controllers
 
                 SecretClient client =
                     new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
-
+                //pass secret name and version
                 var secret = await client.GetSecretAsync(configuration.GetSection("SecretName").Value,
                   configuration.GetSection("SecreteVersion").Value);
                 model.SecretValue = secret.Value.Value;
@@ -64,12 +65,48 @@ namespace LearnSmartCoding.EssentialProducts.API.Controllers
         {
             var model = new KeyVaultModel()
             {
-                KeyVaultURL= configuration.GetSection("KeyVaultURL").Value,
+                KeyVaultURL = configuration.GetSection("KeyVaultURL").Value,
+                SecretName = "SecretNumber",
                 SecretValue = configuration.GetSection("SecretNumber").Value
             };
             return Ok(model);
         }
 
+        [HttpGet("GetReadKey", Name = "GetReadKey")]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        public IActionResult GetReadKey()
+        {
+            string keyName = "key-2";//"key-name";
+            var model = new KeyVaultModel()
+            {
+                KeyVaultURL = "https://lsckv3.vault.azure.net"//configuration.GetSection("KeyVaultURL").Value
+            };
+
+            var client = new KeyClient(new Uri(model.KeyVaultURL), new DefaultAzureCredential());
+
+            var key = client.GetKeyAsync(keyName).Result;
+            model.KeyProperties = key.Value.Properties;
+
+            return Ok(model);
+        }
+
+        [HttpGet("GetReadCertificate", Name = "GetReadCertificate")]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        public IActionResult GetReadCertificate()
+        {
+            string certificateName = "lsc-cert";
+            var model = new KeyVaultModel()
+            {
+                KeyVaultURL = "https://lsckv3.vault.azure.net"//configuration.GetSection("KeyVaultURL").Value
+            };
+
+            var kvUri = model.KeyVaultURL;
+            var client = new CertificateClient(new Uri(kvUri), new DefaultAzureCredential());
+            var certificate = client.GetCertificateAsync(certificateName).Result;
+            model.CertificateValue = certificate.Value.SecretId;
+
+            return Ok(model);
+        }
 
     }
 
@@ -78,6 +115,8 @@ namespace LearnSmartCoding.EssentialProducts.API.Controllers
         public string KeyVaultURL { get; set; }
         public string SecretName { get; set; }
         public string SecretValue { get; set; }
+        public Uri CertificateValue { get; set; }
+        public KeyProperties KeyProperties { get; set; }
         public string  ErrorMessage { get; set; }
     }
 }
